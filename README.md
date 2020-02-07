95c54cd1e999cb60133d7f06abd2c63874b8e445
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-07 20:02:05 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# admin-photobooth-event
+
+## Instalasi
+
+Jalankan perintah di bawah di folder aplikasi:
+
+```
+mim app install admin-photobooth-event
+```
---
+++ modules/admin-photobooth-event/config.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    '__name' => 'admin-photobooth-event',
+    '__version' => '0.0.1',
+    '__git' => 'git@github.com:getmim/admin-photobooth-event.git',
+    '__license' => 'MIT',
+    '__author' => [
+        'name' => 'Iqbal Fauzi',
+        'email' => 'iqbalfawz@gmail.com',
+        'website' => 'http://iqbalfn.com/'
+    ],
+    '__files' => [
+        'modules/admin-photobooth-event' => ['install','update','remove']
+    ],
+    '__dependencies' => [
+        'required' => [
+            [
+                'admin' => NULL
+            ],
+            [
+                'admin-photobooth' => NULL
+            ],
+            [
+                'lib-form' => NULL
+            ],
+            [
+                'lib-formatter' => NULL
+            ]
+        ],
+        'optional' => []
+    ],
+    'autoload' => [
+        'classes' => [],
+        'files' => []
+    ],
+    'libForm' => [
+        'forms' => [
+            'admin.photobooth.index' => [
+                'event' => [
+                    'label' => 'Event',
+                    'type' => 'select',
+                    'sl-filter' => [
+                        'route' => 'adminObjectFilter',
+                        'params' => [],
+                        'query' => ['type' => 'event']
+                    ],
+                    'rules' => []
+                ]
+            ],
+            'admin.photobooth.edit' => [
+                'event' => [
+                    'label' => 'Event',
+                    'type' => 'select',
+                    'sl-filter' => [
+                        'route' => 'adminObjectFilter',
+                        'params' => [],
+                        'query' => ['type' => 'event']
+                    ],
+                    'rules' => [
+                        'exists' => [
+                            'model' => 'Event\Model\Event',
+                            'field' => 'id'
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
